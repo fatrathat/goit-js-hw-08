61c6eff96ceeca934317433f7954da5ae9545a3b
Eugene <eugene.multan@gmail.com>
2022-05-07 13:44:11 +0300

    task 2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+const _ = require('lodash');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const saveCurrentTime = (key, value) => {
+  try {
+    const stateNow = JSON.stringify(value);
+    localStorage.setItem(key, stateNow);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+const loadCurrentTime = key => {
+  try {
+    const stateNow = localStorage.getItem(key);
+    return stateNow === null ? undefined : JSON.parse(stateNow);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const onPlay = data => {
+  saveCurrentTime('videoplayer-current-time', data.seconds);
+};
+
+player.setCurrentTime(loadCurrentTime('videoplayer-current-time'));
+player.on(
+  'timeupdate',
+  _.throttle(data => {
+    onPlay(data);
+  }, 1000),
+);
